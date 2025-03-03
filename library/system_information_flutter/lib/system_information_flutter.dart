bc3bbfc8ac58fc33f5bdb307832c915a14c68bb3
‎ <>
2025-03-03 11:34:59 +0700

    up

--- library/system_information_flutter/lib/system_information_flutter.dart
+++ library/system_information_flutter/lib/system_information_flutter.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 
 /// General Library Documentation Undocument By General Corporation & Global Corporation & General Developer
 class SystemInformationFlutter {
-/// General Library Documentation Undocument By General Corporation & Global Corporation & General Developer
+  /// General Library Documentation Undocument By General Corporation & Global Corporation & General Developer
   static void ensureInitialized() {
     WidgetsFlutterBinding.ensureInitialized();
   }
